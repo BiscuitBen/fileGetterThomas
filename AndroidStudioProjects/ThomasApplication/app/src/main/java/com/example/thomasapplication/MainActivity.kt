e948b5e7a9fe0e2f94bfd4a481a25d814b4318c7
Björn Ruthotto <bjoern.ruthotto@telekom.de>
2023-03-13 15:06:58 +0100

    fehler behebung

--- AndroidStudioProjects/ThomasApplication/app/src/main/java/com/example/thomasapplication/MainActivity.kt
+++ AndroidStudioProjects/ThomasApplication/app/src/main/java/com/example/thomasapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.thomasapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TimePicker
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         val scheduleDownload = findViewById<Button>(R.id.download_button)
         scheduleDownload.setOnClickListener{
+            Log.e("sinal","es funktioniert")
             downloadScheduler()
         }
 
@@ -22,8 +24,13 @@ class MainActivity : AppCompatActivity() {
 
     fun downloadScheduler(){
         val timePicker = findViewById<TimePicker>(R.id.timePicker)
+        Log.e("sinal","timePicker: $timePicker")
+
         val scheduledTime = setTime(timePicker)
         val currentTime = Calendar.getInstance().time
+        Log.e("sinal","currentTime: $currentTime")
+        Log.e("sinal","scheduledTime: ${scheduledTime.time}")
+
         if (currentTime >= scheduledTime.time) {
             val urlEditText = findViewById<EditText>(R.id.url_input)
             val url = urlEditText.text.toString()
--- AndroidStudioProjects/ThomasApplication/app/src/main/java/com/example/thomasapplication/setTime.kt
+++ AndroidStudioProjects/ThomasApplication/app/src/main/java/com/example/thomasapplication/setTime.kt
@@ -6,7 +6,8 @@ import java.util.Calendar
 fun setTime(timePicker: TimePicker): Calendar {
     val calendar = Calendar.getInstance()
     calendar.set(Calendar.HOUR_OF_DAY, timePicker.hour)
-    calendar.set(Calendar.MINUTE, timePicker.hour)
+    // hier hattest du .hour statt .minute
+    calendar.set(Calendar.MINUTE, timePicker.minute)
     calendar.set(Calendar.SECOND, 0)
     calendar.set(Calendar.MILLISECOND, 0)
     return calendar
