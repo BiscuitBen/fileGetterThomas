e83c968d1d4878fa2c1a3c7519347ffef4ba0382
Björn Ruthotto <bjoern.ruthotto@telekom.de>
2023-03-14 10:35:56 +0100

    newwww

--- AndroidStudioProjects/ThomasApplication/app/src/main/java/com/example/thomasapplication/MainActivity.kt
+++ AndroidStudioProjects/ThomasApplication/app/src/main/java/com/example/thomasapplication/MainActivity.kt
@@ -6,9 +6,13 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TimePicker
+import java.io.File
+import java.net.URL
+import java.net.URLConnection
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    var toggle = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,27 +20,68 @@ class MainActivity : AppCompatActivity() {
         val scheduleDownload = findViewById<Button>(R.id.download_button)
         scheduleDownload.setOnClickListener{
             Log.e("sinal","es funktioniert")
+            toggle = true
             downloadScheduler()
         }
 
     }
 
 
-    fun downloadScheduler(){
+    private fun downloadScheduler(){
         val timePicker = findViewById<TimePicker>(R.id.timePicker)
         Log.e("sinal","timePicker: $timePicker")
 
         val scheduledTime = setTime(timePicker)
-        val currentTime = Calendar.getInstance().time
-        Log.e("sinal","currentTime: $currentTime")
-        Log.e("sinal","scheduledTime: ${scheduledTime.time}")
 
-        if (currentTime >= scheduledTime.time) {
-            val urlEditText = findViewById<EditText>(R.id.url_input)
-            val url = urlEditText.text.toString()
-            val path = "/storage/emulated/0/Download/"
-            downloadFile(url, path )
+        while (toggle) {
+            val currentTime = Calendar.getInstance().time
+            //Log.e("sinal", "currentTime: $currentTime")
+            //Log.e("sinal", "scheduledTime: ${scheduledTime.time}")
+
+
+
+            if (currentTime >= scheduledTime.time) {
+                toggle =false
+                val urlEditText = findViewById<EditText>(R.id.url_input)
+                //val url = urlEditText.text.toString()
+                val url = "https://www.orimi.com/pdf-test.pdf"
+                val path = this.getExternalFilesDir(null)?.absolutePath + "/Downsloads/pdf-test.pdf"
+                Log.e("sinal", "url: $url")
+                Log.e("path", "path: $path")
+
+                Thread(Runnable {
+                downloadFile(url, path)
+            }).start()
+
+            }
         }
     }
+    private fun setTime(timePicker: TimePicker): Calendar {
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, timePicker.hour)
+        // hier hattest du .hour statt .minute
+        calendar.set(Calendar.MINUTE, timePicker.minute)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+        return calendar
+    }
 
+    fun downloadFile(urlString: String, savePath: String) {
+        val url = URL(urlString)
+        val connection: URLConnection = url.openConnection()
+        connection.connect()
+        val inputStream = connection.getInputStream()
+        val file = File(savePath)
+        if (!file.isFile) {
+            Log.e("path","The specified path is not a valid file path.")
+        }else{
+
+            file.outputStream().use { outputStream ->
+                inputStream.copyTo(outputStream)
+            }
+            inputStream.close()
+        }
+    }
 }
+
+
--- AndroidStudioProjects/ThomasApplication/app/src/main/java/com/example/thomasapplication/downloadFile.kt
+++ AndroidStudioProjects/ThomasApplication/app/src/main/java/com/example/thomasapplication/downloadFile.kt
@@ -1,5 +1,6 @@
 package com.example.thomasapplication
 
+import android.util.Log
 import java.net.URL
 import java.net.URLConnection
 import java.io.File
@@ -10,8 +11,13 @@ fun downloadFile(urlString: String, savePath: String) {
     connection.connect()
     val inputStream = connection.getInputStream()
     val file = File(savePath)
+    if (!file.isFile) {
+        Log.e("path","The specified path is not a valid file path.")
+    }else{
+
     file.outputStream().use { outputStream ->
         inputStream.copyTo(outputStream)
     }
     inputStream.close()
+    }
 }
